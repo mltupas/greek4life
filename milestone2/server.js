42c2c5f13e330a2306966cb309a8ae9611e8dc87
greek4life <49660510+greek4life@users.noreply.github.com>
2019-05-01 11:45:05 -0700

    Updated server.js

--- milestone2/server.js
+++ milestone2/server.js
@@ -7,18 +7,23 @@ const port = 3000;
 app.use(express.static('static_files'));
 
 const fakeDatabase = {
-    'San Diego': {name: 'Gucci Mane', genre: 'trap'},
+    'La Jolla': {name: 'Gucci Mane', genre: 'Trap', song: 'Wake Up in the Sky', albumCover: 'guccimane.jpg'},
+    'Los Angeles': {name: 'Childish Gambino', genre: 'Trap', song: 'Feels Like Summer', albumCover: 'childishgambino.jpg'},
+    'Irvine': {name: 'Bryan Kearney', genre: 'Electronic', song: 'All Over Again', albumCover: 'bryankearney.jpg'},
+    'Riverside': {name: 'Da Tweekaz', genre: 'Electronic', song: 'Tequila', albumCover: 'datweekaz.jpg'},
+    'Fresno': {name: 'Keith Urban', genre: 'Country', song: 'Somebody Like You', albumCover: 'keithurban.jpg'},
+    'San Diego': {name: 'Billy Ray Cyrus', genre: 'Country', song: 'Achy Breaky Heart', albumCover: 'billyraycyrus.jpg'}
 };
 
 //List of all items in database
-app.get('/items', (req, res) => {
+app.get('/locations', (req, res) => {
     const allItems = Object.keys(fakeDatabase);
     console.log('allItems is: ' + allItems);
     res.send(allItems);
 });
 
 //Get data on an item
-app.get('/items/:itemid', (req, res) => {
+app.get('/location/:itemid', (req, res) => {
     const itemToLookup = req.params.itemid; //matches itemid above
     const val = fakeDatabase[itemToLookup];
     console.log(itemToLookup, '->', val); //debugging
