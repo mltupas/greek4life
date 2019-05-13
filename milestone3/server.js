dcf06f170a1fd564c1ffc72d4e79811750f92faa
greek4life <49660510+greek4life@users.noreply.github.com>
2019-05-12 18:21:57 -0700

    updated server.js

--- milestone3/server.js
+++ milestone3/server.js
@@ -1,39 +1,247 @@
-const express = require('express');
+const express = require('express'); // Express web server framework
+const request = require('request'); // "Request" library
+const querystring = require('querystring');
+
 const app = express();
-const port = 3000;
 
-//use special string called static_files to denote sub-dir
-//if incoming web requests asks for any file present in static_files, fetch file
-app.use(express.static('static_files'));
+const spotifyBaseUrl = 'https://api.spotify.com/v1/';
 
-//const fakeDatabase = {
-//    'La Jolla': {name: 'Gucci Mane', genre: 'Trap', song: 'Wake Up in the Sky', albumCover: 'guccimane.jpg'},
-//    'Los Angeles': {name: 'Childish Gambino', genre: 'Trap', song: 'Feels Like Summer', albumCover: 'childishgambino.jpg'},
-//    'Irvine': {name: 'Bryan Kearney', genre: 'Electronic', song: 'All Over Again', albumCover: 'bryankearney.jpg'},
-//    'Riverside': {name: 'Da Tweekaz', genre: 'Electronic', song: 'Tequila', albumCover: 'datweekaz.jpg'},
-//    'Fresno': {name: 'Keith Urban', genre: 'Country', song: 'Somebody Like You', albumCover: 'keithurban.jpg'},
-//    'San Diego': {name: 'Billy Ray Cyrus', genre: 'Country', song: 'Achy Breaky Heart', albumCover: 'billyraycyrus.jpg'}
-//};
+app.use(express.static(__dirname + '/'));
 
-//List of all items in database
-//app.get('/locations', (req, res) => {
-//    const allItems = Object.keys(fakeDatabase);
-//    console.log('allItems is: ' + allItems);
-//    res.send(allItems);
-//});
+app.get('/user', function(req, res) {
 
-//Get data on an item
-//app.get('/location/:itemid', (req, res) => {
-//    const itemToLookup = req.params.itemid; //matches itemid above
-//    const val = fakeDatabase[itemToLookup];
-//    console.log(itemToLookup, '->', val); //debugging
-//    if (val) {
-//        res.send(val);
-//    } else {
-//        res.send({}); //failed, returning empty object
-//    }
-//});
+  let token = req.query.token;
 
-app.listen(port, () => {
-    console.log('Server started at ' + port);
+  let requestURL = spotifyBaseUrl + 'me';
+
+  let options = {
+    url: requestURL,
+    headers: { 'Authorization': 'Bearer ' + token },
+    json: true
+  };
+
+  request.get(options, function(error, response, body) {
+    res.json(body);
+  });
 });
+
+app.get('/devices', function(req, res) {
+
+  let token = req.query.token;
+
+  let requestURL = spotifyBaseUrl + 'me/player/devices';
+
+  let options = {
+    url: requestURL,
+    headers: { 'Authorization': 'Bearer ' + token },
+    json: true
+  };
+
+  request.get(options, function(error, response, body) {
+    res.send(body.devices);
+  });
+});
+
+app.post('/transfer', function(req, res) { 
+
+  let device_id = req.query.device_id;
+  let token = req.query.token;
+
+  let requestURL = spotifyBaseUrl + 'me/player';
+  
+  let options = {
+    url: requestURL,
+    headers: { 'Authorization': 'Bearer ' + token, 'Content-Type': 'application/json' },
+    json: true,
+    dataType: 'json',
+    body: { "device_ids": [device_id] }
+  };
+  
+  request.put(options, function(error, response, body) {
+    res.sendStatus(200);
+  });
+});
+
+app.get('/genres', function(req, res) {
+
+  let token = req.query.token;
+
+  let requestURL = spotifyBaseUrl + 'recommendations/available-genre-seeds';
+
+  let options = {
+    url: requestURL,
+    headers: { 'Authorization': 'Bearer ' + token },
+    json: true
+  };
+
+  request.get(options, function(error, response, body) {
+    res.json(body.genres);
+  });
+});
+
+app.get('/search', function(req, res) {
+  
+  // Get token and remove from query object
+  let token = req.query.token;
+  delete req.query.token;
+
+  let requestURL = spotifyBaseUrl + 'search?' + 
+  querystring.stringify({
+    limit: 1,
+    market: 'from_token'
+  }) + '&' +
+  querystring.stringify(req.query);
+
+  let options = {
+    url: requestURL,
+    headers: { 'Authorization': 'Bearer ' + token },
+    json: true
+  };
+
+  request.get(options, function(error, response, body) {
+    res.json(body);
+  });
+});
+
+
+app.get('/recommendations', function(req, res) {
+  
+  // Get token and remove from query object
+  let token = req.query.token;
+  delete req.query.token;
+
+  let requestURL = spotifyBaseUrl + 'recommendations?' + 
+  querystring.stringify({
+    limit: 20,
+    market: 'from_token'
+  }) + '&' +
+  querystring.stringify(req.query);
+
+  let options = {
+    url: requestURL,
+    headers: { 'Authorization': 'Bearer ' + token },
+    json: true
+  };
+
+  request.get(options, function(error, response, body) {
+    res.json(body);
+  });
+});
+
+app.get('/tracks', function(req, res) {
+
+  let ids = req.query.ids;
+  let token = req.query.token;
+
+  let requestURL = spotifyBaseUrl + 'tracks?' + 
+  querystring.stringify({
+    ids: ids,
+    market: 'from_token'
+  });
+
+  let options = {
+    url: requestURL,
+    headers: { 'Authorization': 'Bearer ' + token },
+    json: true
+  };
+
+  request.get(options, function(error, response, body) {
+    res.json(body.tracks);
+  });
+});
+
+app.post('/playlist', function(req, res) {
+
+  let tracks = req.query.tracks;
+  let genres = req.query.genres;
+  let token = req.query.token;
+  let features = req.query.features;
+  let userId, playlistUrl;
+
+  // 1. Get user ID
+  let requestURL = spotifyBaseUrl + 'me';
+
+  let options = {
+    url: requestURL,
+    headers: { 'Authorization': 'Bearer ' + token },
+    json: true
+  };
+
+  request.get(options, function(error, response, body) {
+    userId = body.id;
+    
+    // 2. Create playlist
+    requestURL = spotifyBaseUrl + 'users/' + userId + '/playlists';
+
+    options = {
+      url: requestURL,
+      headers: { 'Authorization': 'Bearer ' + token, 'Content-Type': 'application/json' },
+      json: true,
+      dataType: 'json',
+      body: { "name": "Recommended Tracks", "description": "Recommended tracks based on " + genres + " with " + features }
+    };
+
+    request.post(options, function(error, response, body) {
+      playlistUrl = body.tracks.href;
+      
+      // 3. Add tracks to playlist
+      requestURL = playlistUrl + '/?' +
+      querystring.stringify({
+        uris: tracks
+      });
+
+      options = {
+        url: requestURL,
+        headers: { 'Authorization': 'Bearer ' + token },
+        json: true
+      };
+
+      request.post(options, function(error, response, body) {
+        res.sendStatus(200);
+      });
+    });
+  });
+});
+
+app.post('/play', function(req, res) {
+  let tracks = req.query.tracks;
+  let device_id = req.query.device_id;
+  let token = req.query.token;
+
+  let requestURL = spotifyBaseUrl + 'me/player/play?' +
+  querystring.stringify({
+    device_id: device_id
+  });
+
+  let options = {
+    url: requestURL,
+    headers: { 'Authorization': 'Bearer ' + token, 'Content-Type': 'application/json' },
+    json: true,
+    dataType: 'json',
+    body: { "uris": tracks.split(',') }
+  };
+
+  request.put(options, function(error, response, body) {
+    res.sendStatus(200);
+  });
+});
+
+app.post('/pause', function(req, res) {
+  let token = req.query.token;
+
+  let requestURL = spotifyBaseUrl + 'me/player/pause';
+
+  let options = {
+    url: requestURL,
+    headers: { 'Authorization': 'Bearer ' + token, 'Content-Type': 'application/json' },
+    json: true,
+    dataType: 'json',
+  };
+
+  request.put(options, function(error, response, body) {
+    res.sendStatus(200);
+  });
+});
+
+console.log('Listening on http://localhost:3000/');
+app.listen(3000);
