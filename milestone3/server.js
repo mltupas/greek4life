2387c024df903bf793e26c9c01bae845e0ee8e31
greek4life <49660510+greek4life@users.noreply.github.com>
2019-05-12 19:27:31 -0700

    updated server

--- milestone3/server.js
+++ milestone3/server.js
@@ -6,7 +6,8 @@ const app = express();
 
 const spotifyBaseUrl = 'https://api.spotify.com/v1/';
 
-app.use(express.static(__dirname + '/'));
+// app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/static_files'));
 
 app.get('/user', function(req, res) {
 
@@ -79,18 +80,19 @@ app.get('/genres', function(req, res) {
   });
 });
 
-app.get('/search', function(req, res) {
+app.get('/audio-analysis', function(req, res) {
   
   // Get token and remove from query object
   let token = req.query.token;
   delete req.query.token;
 
-  let requestURL = spotifyBaseUrl + 'search?' + 
-  querystring.stringify({
-    limit: 1,
-    market: 'from_token'
-  }) + '&' +
+  console.log('in /audio-analysis')
+  console.log('req.query: ' + req.query);
+
+  let requestURL = spotifyBaseUrl + 'audio-analysis/' + 
   querystring.stringify(req.query);
+
+  console.log('requestURL: ' + requestURL);
 
   let options = {
     url: requestURL,
@@ -103,6 +105,34 @@ app.get('/search', function(req, res) {
   });
 });
 
+app.get('/search', function(req, res) {
+  
+  // Get token and remove from query object
+  let token = req.query.token;
+  delete req.query.token;
+
+  console.log('in /search')
+  console.log('req.query: ' + req.query.text);
+
+  let requestURL = spotifyBaseUrl + 'search?' + 
+  querystring.stringify({
+    limit: 1,
+    market: 'from_token'
+  }) + '&' +
+  querystring.stringify(req.query);
+
+  console.log('requestURL: ' + requestURL);
+
+  let options = {
+    url: requestURL,
+    headers: { 'Authorization': 'Bearer ' + token },
+    json: true
+  };
+
+  request.get(options, function(error, response, body) {
+    res.json(body);
+  });
+});
 
 app.get('/recommendations', function(req, res) {
   
